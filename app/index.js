8366e632c1275015df3d54bc9ba29546c84e00e8
pana <pana.wang@outlook.com>
2015-05-03 09:29:33 +0800

    initail version

--- app/index.js
+++ app/index.js
@@ -1,11 +1,29 @@
 var generators = require('yeoman-generator');
+var fs = require('fs');
+var path = require('path');
+var basename = path.basename(module.filename);
+var rread = require('readdir-recursive');
+var templateFolder = path.join(__dirname, './templates');
 
 module.exports = generators.Base.extend({
     constructor: function() {
-        // Calling the super constructor is important so our generator is correctly set up
         generators.Base.apply(this, arguments);
     },
-    method1: function() {
-        console.log('method 1 just ran');
+    copying: function() {
+        var that = this;
+        rread
+            .fileSync(templateFolder)
+            .map(function (file) {
+                return path.relative(templateFolder, file)
+            })
+            .forEach(function(file){
+                that.copy(that.templatePath(file), that.destinationPath(file));
+            })
+    },
+    installDepends: function () {
+        var packageJson = require(path.join(templateFolder, 'package.json'));
+        var depends = Object.keys(packageJson.dependencies);
+        // how to specify package version here
+        this.npmInstall(depends);
     }
 });
